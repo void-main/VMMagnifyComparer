dfa424f6c36874b0c50a439033462adae53ad590
Void Main <voidmain1313113@gmail.com>
2014-07-16 08:13:39 +0800

    calculate real image size in NSImageView

---
+++ VMMagnifyComparer/NSImageView+ImageSize.h
@@ -0,0 +1,16 @@
+//
+//  NSImageView+ImageSize.h
+//  VMMagnifyComparerExample
+//
+//  Created by Sun Peng on 14-7-16.
+//  Copyright (c) 2014年 Void Main. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSImageView (ImageSize)
+
+- (CGSize)imageScale;
+- (CGRect)imageRect;
+
+@end
